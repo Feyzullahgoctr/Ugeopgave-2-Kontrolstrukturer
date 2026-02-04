e84b95484b648d11e7fd4ae8dc2f71ce5031bcd6
fgoc <feyzullahgoctr@gmail.com>
2026-02-04 17:32:16 +0300

    uge2

--- src/Ugeopgave2.java
+++ src/Ugeopgave2.java
@@ -1,5 +1,5 @@
 
-void main() {
+void main(String[] args) {
 
     // Simple if/else (opg. 1-2)
     // Opgave 1: Shopping discount
@@ -39,6 +39,83 @@ void main() {
 
     }
 
+    // AND-operator (opg. 3-4)
+    // Opgave 4: Complete student report
+    int score = 65;
+    char grade;
+    String status;
+    String comment;
+
+    if (score > 90) {
+        grade = 'A';
+        comment = "Excellent";
+        status = "PASS";
+    } else if (score > 80) {
+        grade = 'B';
+        comment = "Good";
+        status = "PASS";
+    } else if (score > 70) {
+        grade = 'C';
+        comment = "Satisfactory";
+        status = "PASS";
+    } else if (score > 60) {
+        grade = 'D';
+        comment = "Needs improvement";
+        status = "PASS";
+    } else {
+        grade = 'F';
+        comment = "Failing";
+        status = "FAIL";
+    }
+    System.out.println("=== STUDENT REPORT ===");
+    System.out.println("Score : " + score);
+    System.out.println("Grade : " + grade);
+    System.out.println("Status : " + status);
+    System.out.println("Comment : " + comment);
+    System.out.println("===================");
+
+    // OR-operator (opg. 5-7)
+    // Opgave 6: Movie rating system
+    int ageLimit = 15;
+    int guestAge = 14;
+    boolean hasParentalConsent = true;
+
+    if (guestAge >= ageLimit || (guestAge >= 13 && hasParentalConsent)) {
+        System.out.println("Can watch movie");
+    } else {
+        System.out.println("Cannot watch movie");
+    }
+
+    // Kombinerede operatorer (opg. 8-9)
+    // Opgave 8: Insurance premium calculator
+    boolean hasAccidents = false;
+    boolean riskzone = false;
+    int premium = 5000;
+
+    Scanner scanRisk = new Scanner(System.in);
+    System.out.println("Enter Age : ");
+    int age = scanRisk.nextInt();
+
+    System.out.println("Enter accidents : ");
+    int accidents = scanRisk.nextInt();
+
+    if (accidents > 0) {
+        hasAccidents = true;
+
+        if (accidents > 2) {
+            riskzone = true;
+        }
+    }
+
+    if ((age < 25 || age > 70) || (hasAccidents && accidents > 2) || riskzone) {
+        System.out.println("Higher risk category");
+        premium += 2000;
+    } else {
+        System.out.println("Not in the risk category");
+    }
+    System.out.println("Annual premium: " + premium + " kr");
+
+    //
 
 
 }
